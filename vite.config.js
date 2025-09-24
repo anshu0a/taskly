fd05bb50e30b3ed00d823f22c71c9c0f22e9a6bb
anshu0a <anshukumargupta000@gmail.com>
2025-09-24 16:28:13 +0530

    adding _redirect file  commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
- server: {
-  proxy: {
-    '/api': 'https://tasklyserver-0ux1.onrender.com'
-  }
-}
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://tasklyserver-0ux1.onrender.com",
+        changeOrigin: true,
+        secure: false,
+       
+      },
+    },
+  },
 });
+
 
 
 //   'https://tasklyserver-0ux1.onrender.com'
